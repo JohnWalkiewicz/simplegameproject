c70bb372f168dfb8196e33c24ef5ea0e24ebc7d4
John Walkiewicz <walkiewiczbrenda@gmail.com>
2016-11-17 11:26:17 -0800

    Started on overlap

--- simplegame.js
+++ simplegame.js
@@ -1,8 +1,14 @@
-/*OWL MOVEMENT*/
 var owl = document.getElementById("owl")
 var owlXPosition = Number(owl.getAttribute("x"));
 var owlYPosition = Number(owl.getAttribute("y"));
 
+var mice = document.getElementById("mice");
+var miceX = Number(mice.getAttribute("x"));
+var miceY = Number(mice.getAttribute("y"));
+var miceWidth = Number(mice.getAttribute("width"))
+var miceHeight = Number(mice.getAttribute("height"));
+
+/*OWL MOVEMENT*/
 document.addEventListener("keydown", function(e){
   if(e.keyCode == 37){
     owl.setAttribute("x", owlXPosition - 1)
@@ -23,17 +29,9 @@ document.addEventListener("keydown", function(e){
 });
 
 /*OWL COLLSIONS*/
-var mice = document.getElementById("mice");
-
-
-var miceX = Number(mice.getAttribute("x"));
-var miceY = Number(mice.getAttribute("y"));
-
-var miceWidth = Number(owl.getAttribute("width"))
-var miceHeight = Number(owl.getAttribute("height"));
-
-if(owlXPosition > miceX && owlXPosition < miceX + miceWidth && owlYPosition > miceY &&  owlYPosition < miceY + miceHeight){
-  mice.setAttribute("fill-opacity", "0");
+if(owlXPosition > miceX && owlXPosition < miceX + miceWidth && owlXPosition > miceX && owlYPosition < miceY + miceHeight){
+  mice.setAttribute("opacity", "0");
+  console.log("overlap");
 }
 else{
 
